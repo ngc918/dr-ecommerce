3c1bfa4a61b2a1eb2c6a517aa9b630f8aeba0c1d
ngc.918 <nicolas.g9182@gmail.com>
2023-04-19 23:40:41 -0400

    add protected routes and user registration

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -5,8 +5,9 @@ from . import views
 
 urlpatterns = [
     path('users/login/', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('', views.getRoutes, name="routes"),
+    path('users/register/', views.registerUser, name='register'),
     path('users/profile/', views.getUserProfile, name="users-profile"),
+    path('users/', views.getUsers, name="users"),
     path('products/', views.getProducts, name="products"),
     path('products/<str:pk>', views.getProduct, name="product")
 
