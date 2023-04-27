39cb0b20e1a8f808f0bf73f06458cb624ff9ac89
ngc.918 <nicolas.g9182@gmail.com>
2023-04-26 23:57:08 -0400

    User Login Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,19 +7,26 @@ import {
 	productDetailsReducers,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const reducers = combineReducers({
 	productList: productListReducers,
 	productDetails: productDetailsReducers,
 	cart: cartReducer,
+	userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
 	? JSON.parse(localStorage.getItem("cartItems"))
 	: [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+	? JSON.parse(localStorage.getItem("userInfo"))
+	: null;
+
 const initialState = {
 	cart: { cartItems: cartItemsFromStorage },
+	userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
